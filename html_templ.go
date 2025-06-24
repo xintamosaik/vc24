f95946e796604e668a13164ba499d0487d2f8d15
Ulf Dellbrügge <xintamosaik@gmail.com>
2025-06-24 12:58:18 +0200

    fix: update document title to match application name in HTML templates

--- html_templ.go
+++ html_templ.go
@@ -29,7 +29,7 @@ func html(contents templ.Component) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<!doctype html><html lang=\"en\" style=\"font-family: monospace; max-width: 60ch; margin: 0 auto; padding: 2ch; min-height: 100vh; \"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><title>Document</title><meta name=\"color-scheme\" content=\"light dark\"><meta name=\"description\" content=\"Atomic Application App helps to write custom resumes and cover letters\"><script src=\"/htmx.min.js\"></script><link rel=\"stylesheet\" href=\"/styles.css\"></head><body><br>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<!doctype html><html lang=\"en\" style=\"font-family: monospace; max-width: 60ch; margin: 0 auto; padding: 2ch; min-height: 100vh; \"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><title>Atomic Applications</title><meta name=\"color-scheme\" content=\"light dark\"><meta name=\"description\" content=\"Atomic Application App helps to write custom resumes and cover letters\"><script src=\"/htmx.min.js\"></script><link rel=\"stylesheet\" href=\"/styles.css\"></head><body><br>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
